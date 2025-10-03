20f4a080971579b324924c409543da7be4e0f46c
gianlucafarias <palmiergianluca@gmail.com>
2025-10-03 10:47:57 -0300

    changes ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,20 @@ module.exports = {
       {
         name: 'plataforma-servicios-ceres',
         script: 'node_modules/next/dist/bin/next',
-        args: 'start -p 3012',
-        cwd: '/var/www/ceres',
+        args: 'start',
+        cwd: '/var/www/plataforma-servicios-ceres',
+        env_file: '/var/www/plataforma-servicios-ceres/.env',
         env: {
-          NODE_ENV: 'production'
-        }
+          NODE_ENV: 'production', PORT: 3012
+        },
+        instances: 'max',
+        exec_mode: 'cluster',
+        watch: false,
+        out_file: '/var/log/pm2/plataforma-servicios-ceres-out.log',
+      error_file: '/var/log/pm2/plataforma-servicios-ceres-err.log',
+      merge_logs: true,
+      max_memory_restart: '600M'
       }
+
     ]
   };
